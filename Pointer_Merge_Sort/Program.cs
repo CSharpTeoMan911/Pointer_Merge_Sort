c363cf578f13249c95d775c1c8cfc5cc680cc863
Teodor Mihail <teodormihail07@gmail.com>
2025-02-07 02:35:02 +0000

    * IMPROVED PERFORMANCE * FIXED RANDOM NUMBER GENERATION LOGIC ISSUE

--- Pointer_Merge_Sort/Program.cs
+++ Pointer_Merge_Sort/Program.cs
@@ -7,6 +7,9 @@ using System.Runtime.CompilerServices;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Runtime;
 
 namespace Pointer_Merge_Sort
 {
@@ -26,23 +29,17 @@ namespace Pointer_Merge_Sort
         ///                                                                      ///
         ////////////////////////////////////////////////////////////////////////////
 
-
-
+        private static int number_of_elements = 10;
+        private static int max_recursion = 10;
         private static int recursion = 0;
-        private static int[] millisecond_results = new int[100];
+        private static Random rng = new Random();
 
 
         private static unsafe void Merge_Sort(int* arr, int arr_length)
         {
-
-
             if (arr_length > 1)
             {
-
                 // SORTING PROCEDURE
-
-
-
 
 
                 // THE NUMBER OF ELEMENTS IN THE LEFT HALF OF THE MAIN POINTER "arr"
@@ -72,29 +69,16 @@ namespace Pointer_Merge_Sort
 
 
 
+                // INTEGER THAT IS USED TO TRAVERSE THE ELEMENTS OF THE LEFT HALF OF THE MAIN POINTER BY THE HANDLE AT A MEMORY ADDRESS, AND THAT IS USED FOR ACCESSING THEM
+                int l_arr_sort_index = 0;
 
 
-                // HANDLE THAT IS ALLOCATING AT A MEMORY ADDRESS AN INTEGER THAT IS USED TO TRAVERSE THE ELEMENTS OF THE LEFT HALF OF THE MAIN POINTER DURING THE SORTING PROCEDURE
-                System.Runtime.InteropServices.GCHandle l_arr_sort_index_p_handle = System.Runtime.InteropServices.GCHandle.Alloc(0, System.Runtime.InteropServices.GCHandleType.Pinned);
-
-                // POINTER THAT HAS ALLOCATED THE INTEGER THAT IS USED TO TRAVERSE THE ELEMENTS OF THE LEFT HALF OF THE MAIN POINTER BY THE HANDLE AT A MEMORY ADDRESS, AND THAT IS USED FOR ACCESSING THEM
-                int* l_arr_sort_index = (int*)l_arr_sort_index_p_handle.AddrOfPinnedObject();
+                // INTEGER THAT IS USED TO TRAVERSE THE ELEMENTS OF THE RIGHT HALF OF THE MAIN POINTER BY THE HANDLE AT A MEMORY ADDRESS, AND THAT IS USED FOR ACCESSING THEM
+                int r_arr_sort_index = 0;
 
 
-
-                // HANDLE THAT IS ALLOCATING AT A MEMORY ADDRESS AN INTEGER THAT IS USED TO TRAVERSE THE ELEMENTS OF THE RIGHT HALF OF THE MAIN POINTER DURING THE SORTING PROCEDURE
-                System.Runtime.InteropServices.GCHandle r_arr_sort_index_p_handle = System.Runtime.InteropServices.GCHandle.Alloc(0, System.Runtime.InteropServices.GCHandleType.Pinned);
-
-                // POINTER THAT HAS ALLOCATED THE INTEGER THAT IS USED TO TRAVERSE THE ELEMENTS OF THE RIGHT HALF OF THE MAIN POINTER BY THE HANDLE AT A MEMORY ADDRESS, AND THAT IS USED FOR ACCESSING THEM
-                int* r_arr_sort_index = (int*)r_arr_sort_index_p_handle.AddrOfPinnedObject();
-
-
-
-                // HANDLE THAT IS ALLOCATING AT A MEMORY ADDRESS AN INTEGER THAT IS USED TO TRAVERSE THE ELEMENTS OF THE MAIN POINTER DURING THE SORTING PROCEDURE
-                System.Runtime.InteropServices.GCHandle arr_sort_index_p_handle = System.Runtime.InteropServices.GCHandle.Alloc(0, System.Runtime.InteropServices.GCHandleType.Pinned);
-
-                // POINTER THAT HAS ALLOCATED THE INTEGER THAT IS USED TO TRAVERSE THE ELEMENTS OF THE MAIN POINTER BY THE HANDLE AT A MEMORY ADDRESS, AND THAT IS USED FOR ACCESSING THEM
-                int* arr_sort_index = (int*)arr_sort_index_p_handle.AddrOfPinnedObject();
+                // INTEGER THAT IS USED TO TRAVERSE THE ELEMENTS OF THE MAIN POINTER BY THE HANDLE AT A MEMORY ADDRESS, AND THAT IS USED FOR ACCESSING THEM
+                int arr_sort_index = 0;
 
 
 
@@ -126,74 +110,26 @@ namespace Pointer_Merge_Sort
 
 
 
+                // MERGING PROCEDURE
 
-
-
-
-
-
-
-            // MERGING PROCEDURE
-
-
-
-
-
-
-
-            L_ARR_RECURSION:
-                l_arr[*l_arr_sort_index] = arr[*arr_sort_index];
-
-                if (*l_arr_sort_index + 1 < l_arr_length)
+                while (l_arr_sort_index < l_arr_length)
                 {
-                    l_arr[*l_arr_sort_index + 1] = arr[*arr_sort_index + 1];
-                    ++*l_arr_sort_index;
-                    ++*arr_sort_index;
-                }
-
-                ++*l_arr_sort_index;
-                ++*arr_sort_index;
-
-                if (*l_arr_sort_index < l_arr_length)
-                {
-                    goto L_ARR_RECURSION;
-                }
-
-                
-
-
-            R_ARR_RECURSION:
-                r_arr[*r_arr_sort_index] = arr[*arr_sort_index];
-
-                if (*r_arr_sort_index + 1 < r_arr_length)
-                {
-                    r_arr[*r_arr_sort_index + 1] = arr[*arr_sort_index + 1];
-                    ++*arr_sort_index;
-                    ++*r_arr_sort_index;
+                    l_arr[l_arr_sort_index] = arr[arr_sort_index];
+                   l_arr_sort_index++;
+                   arr_sort_index++;
                 }
 
 
-                ++*arr_sort_index;
-                ++*r_arr_sort_index;
-
-                if (*r_arr_sort_index < r_arr_length)
+                while (r_arr_sort_index < r_arr_length)
                 {
-                    goto R_ARR_RECURSION;
+                    r_arr[r_arr_sort_index] = arr[arr_sort_index];
+                    r_arr_sort_index++;
+                    arr_sort_index++;
                 }
-
-
-                
-
 
 
                 Merge_Sort(l_arr, l_arr_length);
                 Merge_Sort(r_arr, r_arr_length);
-
-
-              
-
-
-                
                 
 
                 while (*l_arr_merge_index < l_arr_length && *r_arr_merge_index < r_arr_length)
@@ -240,15 +176,7 @@ namespace Pointer_Merge_Sort
                 l_arr_merge_index_p_handle.Free();
                 r_arr_merge_index_p_handle.Free();
                 arr_merge_index_p_handle.Free();
-
-
-                l_arr_sort_index_p_handle.Free();
-                r_arr_sort_index_p_handle.Free();
-                arr_sort_index_p_handle.Free();
-
             }
-
-
         }
 
 
@@ -257,81 +185,57 @@ namespace Pointer_Merge_Sort
 
 
 
-        private static unsafe Task<bool> Async_Implementation()
+        private static unsafe void Operation(double[] millisecond_results)
         {
-            // HANDLE THAT IS ALLOCATING THE AN INTEGER THAT IS REPRESENTING THE NUMBER OF ELEMENTS THAT CAN BE STORED WITHIN THE MAIN POINTER
-            System.Runtime.InteropServices.GCHandle arr_lenght_handle = System.Runtime.InteropServices.GCHandle.Alloc(1000000, System.Runtime.InteropServices.GCHandleType.Pinned);
-
-            // POINTER THAT IS STORING THE AN INTEGER THAT IS REPRESENTING THE NUMBER OF ELEMENTS THAT CAN BE STORED WITHIN THE MAIN POINTER, ALLOCATED AT A MEMORY ADDRESS
-            int* arr_lenght = (int*)arr_lenght_handle.AddrOfPinnedObject();
-
-
-
-            // HANDLE THAT IS ALLOCATING A NUMBER OF ELEMENTS THAT CAN BE STORED WITHIN THE MAIN POINTER
-            System.Runtime.InteropServices.GCHandle arr_handle = System.Runtime.InteropServices.GCHandle.Alloc(new int[*arr_lenght], System.Runtime.InteropServices.GCHandleType.Pinned);
-
-            // POINTER THAT IS STORING THE NUMBER OF ELEMENTS THAT CAN BE STORED WITHIN THE MAIN POINTER, ALLOCATED AT A MEMORY ADDRESS
-            int* arr = (int*)arr_handle.AddrOfPinnedObject();
-
-
-
-
-
-            Random r = new Random();
-
-
-
-            for (int i = 0; i < *arr_lenght; i++)
+            if (recursion < max_recursion)
             {
-                int rn = r.Next(int.MaxValue);
+                // HANDLE THAT IS ALLOCATING A NUMBER OF ELEMENTS THAT CAN BE STORED WITHIN THE MAIN POINTER
+                System.Runtime.InteropServices.GCHandle arr_handle = System.Runtime.InteropServices.GCHandle.Alloc(new int[number_of_elements], System.Runtime.InteropServices.GCHandleType.Pinned);
 
-                arr[i] = rn;
+                // POINTER THAT IS STORING THE NUMBER OF ELEMENTS THAT CAN BE STORED WITHIN THE MAIN POINTER, ALLOCATED AT A MEMORY ADDRESS
+                int* arr = (int*)arr_handle.AddrOfPinnedObject();
+
+
+
+                Console.WriteLine("\n\n[ BEGIN RANDOM NUMBER GENERATION ]");
+
+                for (int i = 0; i < number_of_elements; i++)
+                {
+                    int rn = rng.Next(0, 10000000);
+                    arr[i] = rn;
+                }
+
+                Console.WriteLine("[ ENDING RANDOM NUMBER GENERATION ]\n\n");
+
+                DateTime start = DateTime.UtcNow;
+
+
+                Merge_Sort(arr, number_of_elements);
+
+
+                double elapssed = (DateTime.UtcNow - start).TotalMilliseconds;
+
+                Console.WriteLine($"\n\nTIME TOOK TO SORT {number_of_elements} RANDOM INTEGERS: {elapssed} MILLISECONDS");
+                millisecond_results[recursion] = elapssed;
+
+
+                // DEALLOCATING THE HANDLES AND THEIR CONTENT FROM MEMORY
+                arr_handle.Free();
+
+
+                recursion++;
+                Operation(millisecond_results);
             }
-
-          
-
-
-            System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
-            s.Start();
-            Merge_Sort(arr, *arr_lenght);
-            s.Stop();
-
-            Console.WriteLine("TIME TOOK TO SORT " + *arr_lenght + " RANDOM INTEGERS: " + s.ElapsedMilliseconds + " MILLISECONDS");
-
-            millisecond_results[recursion] = (int)s.ElapsedMilliseconds;
-            
-            
-
-
-            // DEALLOCATING THE HANDLES AND THEIR CONTENT FROM MEMORY
-
-            arr_handle.Free();
-            arr_lenght_handle.Free();
-
-
-
-            return Task.FromResult(true);
         }
 
 
-
-        public static async void Implementation()
+        public static void Implementation(double[] millisecond_results)
         {
-            await Async_Implementation();
+            Operation(millisecond_results);
 
-
-
-            recursion++;
-
-            if (recursion < 100)
-            {
-                
-                Implementation();
-            }
-
-            int sum = 0;
-            int fastest = int.MaxValue;
-            int slowest = 0;
+            double sum = 0;
+            double fastest = double.MaxValue;
+            double slowest = 0;
 
             for (int i = 0; i < millisecond_results.Length; i++)
             {
@@ -348,18 +252,29 @@ namespace Pointer_Merge_Sort
                 sum += millisecond_results[i];
             }
 
-            Console.WriteLine("\nThe calculated average time is: " + sum / millisecond_results.Length + " MILLISECONDS\n");
 
-            Console.WriteLine("The fastest time is: " + fastest + " MILLISECONDS\n");
+            Console.WriteLine($"\nThe calculated average time is: {sum / millisecond_results.Length} MILLISECONDS\n");
 
-            Console.WriteLine("The slowest time is: " + slowest + " MILLISECONDS\n");
+            Console.WriteLine($"The fastest time is: {fastest} MILLISECONDS\n");
+
+            Console.WriteLine($"The slowest time is: {slowest} MILLISECONDS\n");
 
             Console.ReadLine();
+
+
         }
 
         public static void Main(String[] args)
         {
-            Implementation();
+            Console.Write("\n Enter the number of sorting operations: ");
+            max_recursion = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n Enter the number of elements to sort: ");
+            number_of_elements = Convert.ToInt32(Console.ReadLine());
+
+            double[] millisecond_results = new double[max_recursion];
+
+            Implementation(millisecond_results);
         }
     }
 }
